f962aadb6e46ab00bdb8e15f236cfa5df538ee2a
vilakazibuyisiwe2-ops <vilakazibuyisiwe2@gmail.com>
2026-06-11 12:52:49 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+function loadAdmin() {
+    document.getElementById("adminName").textContent =
+        "Buyisiwe Vilakazi";
 
+    document.getElementById("phone").textContent =
+        "076 XXX XXXX";
+
+    document.getElementById("email").textContent =
+        "vilakazibuyisiwe2@gmail.com";
+
+    document.getElementById("address").textContent =
+        "Johannesburg, South Africa";
+}
